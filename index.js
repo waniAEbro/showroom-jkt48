b2febd28304fcf3573647efdd78d2622ec9b751d
Ridwan Ridlo <murtiniibu110@gmail.com>
2022-12-10 06:44:45 +0700

    update

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const layouts = require("express-ejs-layouts");
 const methodOverride = require("method-override");
 const http = require("http");
+const https = require("https");
 const { Server } = require("socket.io");
 const {
     Client,
     LocalAuth
 } = require('whatsapp-web.js');
 const qrcode = require('qrcode');
+const { resolve } = require("path");
+const id_profil = [318209, 318118, 318204, 318117, 318207, 400713, 318120, 400717, 318208, 317727, 318112, 318112, 317738, 318232, 318222, 318219, 400712, 400716, 400710, 318218, 400715, 318210, 400718, 318227, 318225, 318228, 318224, 317739, 400714, 318230, 318223, 318233, 318229];
+let lagi_live = [];
 
 const app = express();
 const port = 3000;
@@ -20,9 +24,16 @@ app.set("view engine", "ejs");
 
 app.use(layouts);
 
-app.use(express.static("public"));
+app.use(express.static("public/"));
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methids", "GET, POST, PUT, PATCH, DELTE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 
 app.use(methodOverride("_method"));
 
@@ -31,8 +42,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/qr", (req, res) => {
-    res.render("whatsapp", { layout: "layouts/main", title: "Home" });
+    res.render("whatsapp", { layout: "layouts/main", title: "Whatsapp" });
 });
+
+app.get("/nonton", (req, res) => {
+    res.render("nonton", { layout: "layouts/main", title: "Nonton" })
+})
+
+app.get("/:id", (req, res) => {
+    res.render("detail", { layout: "layouts/main", title: "Detail", member: req.params.id })
+})
 
 io.on("connection", (socket) => {
     socket.emit("message", "Loading ... ")
