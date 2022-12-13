78c3d8822d1d75f249064a39f8771eaa708aeb0f
Ridwan Ridlo <murtiniibu110@gmail.com>
2022-12-13 21:00:07 +0700

    isi

--- controllers/MemberController.js
+++ controllers/MemberController.js
@@ -33,7 +33,30 @@ const getLive = (req, res) => {
 };
 
 const updateDatabase = (req, res) => {
-    res.status(404).render("404", { layout: "layouts/main", title: "404" });
+    let id_profil = [318209, 318118, 318204, 318117, 318207, 400713, 318120, 400717, 318208, 317727, 318112, 317738, 318232, 318222, 318219, 400712, 400716, 400710, 318218, 400715, 318210, 400718, 318227, 318225, 318228, 318224, 317739, 400714, 318230, 318223, 318233, 318229];
+
+    id_profil.forEach(id => {
+        https.get("https://www.showroom-live.com/api/room/profile?room_id=" + id, (response) => {
+            let respond = "";
+
+            response.on("data", data => {
+                respond += data;
+            })
+
+            response.on("end", () => {
+                console.log(JSON.parse(respond))
+                const member = new Member({
+                    info: JSON.parse(respond),
+                    is_notified: false
+                })
+
+                member.save();
+            })
+        }).on("error", error => {
+            console.log(error);
+        });
+    });
+    res.send("halo");
 };
 
 module.exports = {
